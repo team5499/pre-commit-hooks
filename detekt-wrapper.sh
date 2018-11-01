25571568d0701ac0c4459a2383bc020309b39bf3
Connor Worley <cbworley@yelp.com>
2018-10-31 23:26:11 -0700

    Add detekt wrapper

--- detekt-wrapper.sh
+++ detekt-wrapper.sh
@@ -13,11 +13,9 @@ if [ ! -f "$CACHE_DIR/detekt-RC9.2/detekt-cli/build/libs/detekt-cli-1.0.0.RC9.2-
         >&2 echo "Please install curl or wget."
         exit 1
     fi
-    command tar -zxf "RC9.2.tar.gz" 
-    command rm RC9.2.tar.gz
-    command cd "detekt-RC9.2"
-    command ./gradlew build shadowJar
-    command cd ..
+    tar -zxf "$CACHE_DIR/RC9.2.tar.gz"
+    rm "$CACHE_DIR/RC9.2.tar.gz"
+    command "$CACHE_DIR/detekt-RC9.2/gradlew" -q -p "$CACHE_DIR/detekt-RC9.2" shadowJar > /dev/null 2>&1
 fi
 
-command java -jar "$CACHE_DIR/detekt-RC9.2/detekt-cli/build/libs/detekt-cli-1.0.0.RC9.2-all.jar" --debug -i "$@"
+java -jar "$CACHE_DIR/detekt-RC9.2/detekt-cli/build/libs/detekt-cli-1.0.0.RC9.2-all.jar" -i "$@"
