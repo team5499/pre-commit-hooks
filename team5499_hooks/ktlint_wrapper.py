d53c6bd3073922610367f0e0ff1769474df7326b
andycate <and164v@gmail.com>
2019-02-12 22:48:49 -0800

    Add stdout and stderr logging

--- team5499_hooks/ktlint_wrapper.py
+++ team5499_hooks/ktlint_wrapper.py
@@ -6,10 +6,16 @@ def main(argv=[]):
         newArgs = ' '.join(argv)
         p = Popen('ktlint-wrapper.bat ' + newArgs)
         stdout, stderr = p.communicate()
+        print(stdout)
+        print()
+        print(stderr)
     else:
         newArgs = ' '.join(argv)
         p = Popen('ktlint-wrapper.sh ' + newArgs)
         stdout, stderr = p.communicate()
+        print(stdout)
+        print()
+        print(stderr)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
