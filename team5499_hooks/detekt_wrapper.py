a3d6f8414b42485322757064f2492c7c49703ec6
andycate <and164v@gmail.com>
2019-02-12 22:38:30 -0800

    Add fallback for empty args

--- team5499_hooks/detekt_wrapper.py
+++ team5499_hooks/detekt_wrapper.py
@@ -2,17 +2,14 @@ from os import sys
 from subprocess import Popen
 
 def main(argv=None):
-    print(argv)
-    print(sys.argv)
     if(sys.platform == "win32"):
-        newArgs = ' '.join(argv[1:])
+        newArgs = ' '.join(argv)
         p = Popen('detekt-wrapper.bat ' + newArgs)
         stdout, stderr = p.communicate()
     else:
-        newArgs = ' '.join(argv[1:])
+        newArgs = ' '.join(argv)
         p = Popen('detekt-wrapper.sh ' + newArgs)
         stdout, stderr = p.communicate()
 
 if __name__ == '__main__':
-    print("from main")
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
--- team5499_hooks/ktlint_wrapper.py
+++ team5499_hooks/ktlint_wrapper.py
@@ -1,15 +1,15 @@
 from os import sys
 from subprocess import Popen
 
-def main(argv=None):
+def main(argv=[]):
     if(sys.platform == "win32"):
-        newArgs = ' '.join(argv[1:])
+        newArgs = ' '.join(argv)
         p = Popen('ktlint-wrapper.bat ' + newArgs)
         stdout, stderr = p.communicate()
     else:
-        newArgs = ' '.join(argv[1:])
+        newArgs = ' '.join(argv)
         p = Popen('ktlint-wrapper.sh ' + newArgs)
         stdout, stderr = p.communicate()
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
