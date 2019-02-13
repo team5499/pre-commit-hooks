21db6e5e0d29262645241600404dcee2abe133c9
andycate <and164v@gmail.com>
2019-02-12 22:48:49 -0800

    Add stdout and stderr logging

--- team5499_hooks/detekt_wrapper.py
+++ team5499_hooks/detekt_wrapper.py
@@ -6,10 +6,16 @@ def main(argv=[]):
         newArgs = ' '.join(argv)
         p = Popen('detekt-wrapper.bat ' + newArgs)
         stdout, stderr = p.communicate()
+        print(stdout)
+        print()
+        print(stderr)
     else:
         newArgs = ' '.join(argv)
         p = Popen('detekt-wrapper.sh ' + newArgs)
         stdout, stderr = p.communicate()
+        print(stdout)
+        print()
+        print(stderr)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
