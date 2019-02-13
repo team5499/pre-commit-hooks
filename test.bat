731e24a4f1f7fcd458018e31bbd38209c1892699
andycate <and164v@gmail.com>
2019-02-12 22:12:35 -0800

    Convert to python module

--- test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-setlocal enabledelayedexpansion
-set /a count=0
-for %%a in (%*) do (
-    set /a count+=1
-    echo %%a : !count!
-)
-endlocal
--- test.kt
+++
@@ -1,5 +0,0 @@
-package testdeek
-
-class TEsst {
-    zdsgsdgfdgsfdg
-}
