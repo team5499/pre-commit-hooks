ef9f2f315924944d534decad25f529e3957769ab
andycate <and164v@gmail.com>
2019-02-12 22:20:12 -0800

    Fix python module naming

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup
 
 
 setup(
-    name='team5499-hooks',
+    name='team5499_hooks',
     description='team5499 specific kotlin precommit hooks',
     url='https://github.com/team5499/pre-commit-hooks',
-    version='0.3.5',
+    version='0.3.6',
 
-    packages=find_packages(exclude=('tests*', 'testing*')),
+    packages=find_packages(),
     extras_require={':python_version<"3.5"': ['typing']},
     entry_points={
         'console_scripts': [
-            'ktlint-wrapper = team5499-hooks.ktlint-wrapper:main',
-            'detekt-wrapper = team5499-hooks.detekt-wrapper:main'
+            'ktlint_wrapper = team5499_hooks.ktlint_wrapper:main',
+            'detekt_wrapper = team5499_hooks.detekt_wrapper:main'
         ],
     },
 )
