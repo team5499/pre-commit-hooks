c68dce23a5386913639abf5896e969875bc48e57
Andy Cate <and164v@gmail.com>
2019-02-26 18:01:52 -0800

    Test

--- setup.py
+++ setup.py
@@ -6,14 +6,9 @@ setup(
     name='team5499_hooks',
     description='team5499 specific kotlin precommit hooks',
     url='https://github.com/team5499/pre-commit-hooks',
-    version='0.3.13',
+    version='0.3.15',
 
     packages=find_packages(),
-    package_data={'team5499_hooks': ['team5499_hooks/detekt-wrapper.bat',
-                                        'team5499_hooks/detekt-wrapper.sh'
-                                        'team5499_hooks/ktlint-wrapper.bat'
-                                        'team5499_hooks/ktlint-wrapper.sh']},
-    include_package_data=True,
     extras_require={':python_version<"3.5"': ['typing']},
     entry_points={
         'console_scripts': [
--- team5499_hooks/detekt_wrapper.py
+++ team5499_hooks/detekt_wrapper.py
@@ -1,24 +1,67 @@
 import os
 from subprocess import Popen
+import urllib
 
-def main(argv=[]):
-    print("0.3.13")
-    if(os.sys.platform == "win32"):
-        newArgs = ' '.join(argv)
-        executable = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'detekt-wrapper.bat')
-        p = Popen(executable + ' ' + newArgs)
-        stdout, stderr = p.communicate()
-        print(stdout)
-        print()
-        print(stderr)
-    else:
-        newArgs = ' '.join(argv)
-        executable = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'detekt-wrapper.sh')
-        p = Popen('/bin/bash ' + executable + ' ' + newArgs)
-        stdout, stderr = p.communicate()
-        print(stdout)
-        print()
-        print(stderr)
+DOWNLOAD_URL = "https://github.com/arturbosch/detekt/releases/download/RC10/detekt-cli-1.0.0-RC10-all.jar"
+CACHE_DIR=os.path.dirname(os.path.abspath(__file__))
+
+# def runWindows(files, extras):
+#     if(not os.path.exists(os.path.join(CACHE_DIR, "detekt.jar"))):
+#         print("downloading detekt.jar ...")
+#         urllib.urlretrieve(DOWNLOAD_URL, os.path.join(CACHE_DIR, "detekt.jar"))
+    
+#     p = Popen('java -jar ' + os.path.join(CACHE_DIR, "detekt.jar") + ' ' + ' '.join(extras) + ' -i ' + files)
+#     stdout, stderr = p.communicate()
+#     print(stdout)
+#     print()
+#     print(stderr)
+
+# def runNix(files, extras):
+#     if(not os.path.exists(os.path.join(CACHE_DIR, "detekt.jar"))):
+#         print("downloading detekt.jar ...")
+#         urllib.urlretrieve(DOWNLOAD_URL, os.path.join(CACHE_DIR, "detekt.jar"))
+    
+    
+
+# def main(files, extras):
+#     print("0.3.15")
+#     if(os.sys.platform == "win32"):
+#         # newArgs = ' '.join(argv)
+#         # executable = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'detekt-wrapper.bat')
+#         # p = Popen(executable + ' ' + newArgs)
+#         # stdout, stderr = p.communicate()
+#         # print(stdout)
+#         # print()
+#         # print(stderr)
+#         runWindows(files, extras)
+#     else:
+#         # newArgs = ' '.join(argv)
+#         # executable = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'detekt-wrapper.sh')
+#         # p = Popen('/bin/bash ' + executable + ' ' + newArgs)
+#         # stdout, stderr = p.communicate()
+#         # print(stdout)
+#         # print()
+#         # print(stderr)
+#         runNix(files, extras)
 
 if __name__ == '__main__':
-    os.sys.exit(main(os.sys.argv[1:]))
+    print("0.3.15")
+    args = os.sys.argv[1:]
+    files = ''
+    extras = []
+    for i in args:
+        if(os.path.exists(i)):
+            files = files + ',' + i
+        else:
+            extras.append(i)
+    
+    if(not os.path.exists(os.path.join(CACHE_DIR, "detekt.jar"))):
+        print("downloading detekt.jar ...")
+        urllib.urlretrieve(DOWNLOAD_URL, os.path.join(CACHE_DIR, "detekt.jar"))
+    
+    p = Popen('java -jar ' + os.path.join(CACHE_DIR, "detekt.jar") + ' ' + ' '.join(extras) + ' -i ' + files[1:], shell=True)
+    stdout, stderr = p.communicate()
+    print(stdout)
+    print(stderr)
+    print('Finished')
+    os.sys.exit(p.returncode)
