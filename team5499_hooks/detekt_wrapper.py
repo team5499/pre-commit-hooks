e75341ad8346e6e3cbd200d32672c724c93f97f9
Andy Cate <and164v@gmail.com>
2019-02-26 18:46:51 -0800

    Add ktlint wrapper

--- team5499_hooks/detekt_wrapper.py
+++ team5499_hooks/detekt_wrapper.py
@@ -8,45 +8,6 @@ except ImportError:
 DOWNLOAD_URL = "https://github.com/arturbosch/detekt/releases/download/RC10/detekt-cli-1.0.0-RC10-all.jar"
 CACHE_DIR=os.path.dirname(os.path.abspath(__file__))
 
-# def runWindows(files, extras):
-#     if(not os.path.exists(os.path.join(CACHE_DIR, "detekt.jar"))):
-#         print("downloading detekt.jar ...")
-#         urllib.urlretrieve(DOWNLOAD_URL, os.path.join(CACHE_DIR, "detekt.jar"))
-    
-#     p = Popen('java -jar ' + os.path.join(CACHE_DIR, "detekt.jar") + ' ' + ' '.join(extras) + ' -i ' + files)
-#     stdout, stderr = p.communicate()
-#     print(stdout)
-#     print()
-#     print(stderr)
-
-# def runNix(files, extras):
-#     if(not os.path.exists(os.path.join(CACHE_DIR, "detekt.jar"))):
-#         print("downloading detekt.jar ...")
-#         urllib.urlretrieve(DOWNLOAD_URL, os.path.join(CACHE_DIR, "detekt.jar"))
-    
-    
-
-# def main(files, extras):
-#     print("0.3.15")
-#     if(os.sys.platform == "win32"):
-#         # newArgs = ' '.join(argv)
-#         # executable = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'detekt-wrapper.bat')
-#         # p = Popen(executable + ' ' + newArgs)
-#         # stdout, stderr = p.communicate()
-#         # print(stdout)
-#         # print()
-#         # print(stderr)
-#         runWindows(files, extras)
-#     else:
-#         # newArgs = ' '.join(argv)
-#         # executable = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'detekt-wrapper.sh')
-#         # p = Popen('/bin/bash ' + executable + ' ' + newArgs)
-#         # stdout, stderr = p.communicate()
-#         # print(stdout)
-#         # print()
-#         # print(stderr)
-#         runNix(files, extras)
-
 if __name__ == '__main__':
     print("0.3.15")
     args = os.sys.argv[1:]
--- team5499_hooks/ktlint_wrapper.py
+++ team5499_hooks/ktlint_wrapper.py
@@ -1,24 +1,33 @@
 import os
 from subprocess import Popen
+try:
+    from urllib import urlretrieve
+except ImportError:
+    from urllib.request import urlretrieve
 
-def main(argv=[]):
-    print("0.3.15")
-    if(os.sys.platform == "win32"):
-        newArgs = ' '.join(argv)
-        executable = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ktlint-wrapper.bat')
-        p = Popen(executable + ' ' + newArgs)
-        stdout, stderr = p.communicate()
-        print(stdout)
-        print()
-        print(stderr)
-    else:
-        newArgs = ' '.join(argv)
-        executable = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ktlint-wrapper.sh')
-        p = Popen('/bin/bash ' + executable + ' ' + newArgs)
-        stdout, stderr = p.communicate()
-        print(stdout)
-        print()
-        print(stderr)
+DOWNLOAD_URL = "https://github.com/shyiko/ktlint/releases/download/0.29.0/ktlint"
+CACHE_DIR=os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
-    os.sys.exit(main(os.sys.argv[1:]))
+    print("0.3.15")
+    args = os.sys.argv[1:]
+    if(not os.path.exists(os.path.join(CACHE_DIR, "ktlint"))):
+        print("downloading ktlint ...")
+        urlretrieve(DOWNLOAD_URL, os.path.join(CACHE_DIR, "ktlint"))
+    
+    if(os.sys.platform == "win32"):
+        p = Popen('C:\\Program Files\\Git\\bin\\sh.exe' + os.path.join(CACHE_DIR, "ktlint") + ' ' + ' '.join(args), shell=True)
+        stdout, stderr = p.communicate()
+        print(stdout)
+        print(stderr)
+        print('Finished')
+        os.sys.exit(p.returncode)
+    else:
+        print("command")
+        p = Popen('chmod a+x ' + os.path.join(CACHE_DIR, "ktlint") + ' && command ' + os.path.join(CACHE_DIR, "ktlint") + ' ' + ' '.join(args), shell=True)
+        stdout, stderr = p.communicate()
+        print(stdout)
+        print(stderr)
+        print('Finished')
+        os.sys.exit(p.returncode)
+    
