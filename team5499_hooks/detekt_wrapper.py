7e1df70fdc3f5770efe6a6b2b66e11100599217f
Team5499Admin <bhsrobot@gmail.com>
2019-02-26 18:26:22 -0800

    works on windows!

--- team5499_hooks/detekt_wrapper.py
+++ team5499_hooks/detekt_wrapper.py
@@ -1,6 +1,9 @@
 import os
 from subprocess import Popen
-import urllib
+try:
+    from urllib import urlretrieve
+except ImportError:
+    from urllib.request import urlretrieve
 
 DOWNLOAD_URL = "https://github.com/arturbosch/detekt/releases/download/RC10/detekt-cli-1.0.0-RC10-all.jar"
 CACHE_DIR=os.path.dirname(os.path.abspath(__file__))
@@ -57,7 +60,7 @@ if __name__ == '__main__':
     
     if(not os.path.exists(os.path.join(CACHE_DIR, "detekt.jar"))):
         print("downloading detekt.jar ...")
-        urllib.urlretrieve(DOWNLOAD_URL, os.path.join(CACHE_DIR, "detekt.jar"))
+        urlretrieve(DOWNLOAD_URL, os.path.join(CACHE_DIR, "detekt.jar"))
     
     p = Popen('java -jar ' + os.path.join(CACHE_DIR, "detekt.jar") + ' ' + ' '.join(extras) + ' -i ' + files[1:], shell=True)
     stdout, stderr = p.communicate()
